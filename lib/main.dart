a9316c8b41180d4ad713da5516c09950a51fbb42
Ali Ghahremani <alighahremani1377@gmail.com>
2022-06-12 00:20:01 +0430

    feature: start using websocket. feature: reconnect mechanism

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_client/build_configs.dart';
 import 'package:flutter_client/fake_data.dart';
 import 'package:flutter_client/repo/local/contact_repo.dart';
+import 'package:flutter_client/repo/remote/web_socket_service.dart';
 import 'package:flutter_client/view/main_layout.dart';
 import 'package:flutter_client/view_model/contact_view_model.dart';
 
 void main() {
   // start services here
+
+  //web socket service
+  WebSocketService.init();
+
+  //local data providers
   if(BuildConfig.fake_data) {
     FakeData.load();
   } else {
--- lib/repo/remote/web_socket_service.dart
+++ lib/repo/remote/web_socket_service.dart
@@ -1,24 +1,63 @@
 
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:flutter_client/view_model/contact_view_model.dart';
+import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class WebSocketService{
 
   static const SERVER_ADDRESS = "ws://127.0.0.1:3000/";
+  static const RECONNECT_TIME = 1000;
 
   static WebSocketChannel? channel;
+  static bool isConnect = false;
+  static bool isRegister = false;
 
   static init(){
-
-    channel = WebSocketChannel.connect(
+    connect();
+    reconnectLoop();
+  }
+  static connect(){
+    channel = IOWebSocketChannel.connect(
       Uri.parse(SERVER_ADDRESS),
+      pingInterval: const Duration(milliseconds: 10000)
     );
 
     channel?.stream.listen((message) {
-        onMessage(jsonDecode(message));
-    });
+      onMessage(jsonDecode(message));
+    }, onError: (e){
+      print(e.toString());
+      isConnect = false;
+      isRegister = false;
+    }, onDone: (){
+      // on disconnect
+      print("on done");
+      print("disconnect: ${channel?.closeReason}");
+      isConnect = false;
+      isRegister = false;
+    }, cancelOnError: true);
 
+    isConnect = true;
+  }
+
+  static reconnectLoop(){
+    Timer.periodic(const Duration(milliseconds: RECONNECT_TIME), (timer) {
+      if(!isConnect){
+        connect();
+      }else if(!isRegister){
+        sendJoin(
+            ContactViewModel.contacts.value.map(
+                    (e) => e.roomId
+            ).toList()
+        );
+        isRegister = true;
+      }
+
+
+      //TODO test publish() and check if other side get packet
+    });
   }
 
   static send(String data){
